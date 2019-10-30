0e19b4771177366b8af52ae4a03fc7940a6542f3
KhingNoii <60070079@kmitl.ac.th>
2019-10-30 14:01:47 +0700

    check number of class attribute

--- dataSet/src/neuralModel.py
+++ dataSet/src/neuralModel.py
@@ -128,14 +128,14 @@ normalized = lambda x, maxv, minv : (x-minv*0.95)/(maxv*1.05-minv*0.95)
 # 	[8.675418651,-0.242068655,1],
 # 	[7.673756466,3.508563011,1]]
 
-# data = pd.read_csv("../wdbc.csv", index_col=0)
-data = pd.read_csv("../iris.csv")
+data = pd.read_csv("../wdbc.csv", index_col=0)
+# data = pd.read_csv("../iris.csv")
 num = 0
-max_val, min_val = [0 for i in range(4)], [0 for i in range(4)]
-# max_val, min_val = [0 for i in range(30)], [0 for i in range(30)]
+# max_val, min_val = [0 for i in range(4)], [0 for i in range(4)]
+max_val, min_val = [0 for i in range(30)], [0 for i in range(30)]
 for i in data:
     if(i != 'class'):
-        if(num == 4):
+        if(num == 30):
             break
         max_val[num] = max(data[i])
         min_val[num] = min(data[i])
@@ -149,30 +149,31 @@ for j in ID:
         format_data.append(data[i][j])
     data_key.append(format_data)
 
-num_inputs = len(data_key[0]) 
+
+num_inputs = len(data_key[0])-1 
 print(num_inputs)
 
 # num_outputs = len(set([row[-1] for row in data_key]))
 num_outputs = len(set(data['class']))
 
-print(data_key)
+# print(data_key)
 
 
 
 
 for i in range(len(data_key)):
-    class_val = data_key[i][-1]
-    del data_key[i][-1]
-    # del data_key[i][0]
-    # data_key[i].append(1 if class_val == 'M' else 0)
-    if(class_val == 'Iris-setosa'):
-        data_key[i].append(0)
-    elif(class_val == 'Iris-versicolor'):
-        data_key[i].append(1)
-    elif(class_val == 'Iris-virginica'):
-        data_key[i].append(2)
+    class_val = data_key[i][0]
+    # del data_key[i][-1]
+    del data_key[i][0]
+    data_key[i].append(1 if class_val == 'M' else 0)
+    # if(class_val == 'Iris-setosa'):
+    #     data_key[i].append(0)
+    # elif(class_val == 'Iris-versicolor'):
+    #     data_key[i].append(1)
+    # elif(class_val == 'Iris-virginica'):
+    #     data_key[i].append(2)
 
-print(data_key[0])
+
 # print(max_val)
 # print(min_val[0])
 
@@ -181,41 +182,50 @@ for i in range(len(data_key)):
         
         data_key[i][j] = normalized(data_key[i][j], max_val[j], min_val[j])
 print("After Normalized")
-print(data_key[0])
 
-for i in range(51):
-    print(data_key[i])
+
+# for i in range(51):
+#     print(data_key[i])
 # num_inputs = len(data_key[0]) 
 # num_outputs = len(set([row[-1] for row in data_key]))
 print("Number of Input Layer: ", num_inputs)
 print("Number of Output Layer: ", num_outputs)
 # seed(1)
 
+print(data_key[0])
 shuffle(data_key)
+print(data_key[0])
+num0, num1 = 0, 0
+for i in data_key:
+    if(i[-1] == 0):
+        num0+=1
+    else:
+        num1+=1
 
-
+print(num0)
+print(num1)
 networks = []
 accuracy = []
 # network = NeuralNetwork(num_inputs, 10, num_outputs)
-for i in range(10):
-    networks.append(NeuralNetwork(num_inputs, 10, num_outputs))
-    networks[i].training(data_key, 0.1, 500, num_outputs, 5*i)
+# for i in range(10):
+#     networks.append(NeuralNetwork(num_inputs, 10, num_outputs))
+#     networks[i].training(data_key, 0.1, 500, num_outputs, 5*i)
     
-    num = 0
-    for row in networks[i].testing:
-        prediction = networks[i].predict(row)
-        if row[-1] == prediction:
-            num += 1
-        print("Expect=%d  Output=%d" % (row[-1], prediction))
-    accuracy.append(num/15*100)
+#     num = 0
+#     for row in networks[i].testing:
+#         prediction = networks[i].predict(row)
+#         if row[-1] == prediction:
+#             num += 1
+#         print("Expect=%d  Output=%d" % (row[-1], prediction))
+#     accuracy.append(num/15*100)
 
 
-for i in range(10):
-    print("Model ", i)
-    print(networks[i].network, end='\n\n')
-    print("Accuracy : ", accuracy[i])
+# for i in range(10):
+#     print("Model ", i)
+#     print(networks[i].network, end='\n\n')
+#     print("Accuracy : ", accuracy[i])
 
-print("Mean Accuracy: " ,sum(accuracy)/10)
+# print("Mean Accuracy: " ,sum(accuracy)/10)
   
     
 
